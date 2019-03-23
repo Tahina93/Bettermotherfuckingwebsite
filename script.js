f7ef726fa11977bc03a369a569ecbb6215565b64
Tahina93 <anne.tahina.metz@gmail.com>
2019-03-23 14:11:52 +0100

    ajout de Javascript. Popup créé, manque plus que le lien lorsqu'on clique sur OK

---
+++ script.js
@@ -0,0 +1,8 @@
+setTimeout (myFunction, 10000);
+function myFunction() {
+if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")) {
+  txt = "OK!";
+} else {
+  txt = "Nope!";
+}
+}
